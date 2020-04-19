5583d2f81dfec28ab96c4166ef300db8cd892cff
Hardeep Asrani <hardeepasrani@gmail.com>
2020-04-19 19:01:44 +0530

    Fix phpcs error

--- class-gutenberg-menu-icons.php
+++ class-gutenberg-menu-icons.php
@@ -69,14 +69,14 @@ class GutenbergMenuIcons {
 			'themeisle-gutenberg-menu-icons',
 			plugin_dir_url( $this->get_dir() ) . $this->slug . '/build/editor.css',
 			array( 'font-awesome-5' ),
-			$version,
+			$version
 		);
 
 		wp_enqueue_style(
 			'themeisle-gutenberg-menu-icons-font-awesome',
 			plugins_url( '/', __FILE__ ) . 'assets/css/font-awesome.min.css',
 			array( 'font-awesome-5' ),
-			$version,
+			$version
 		);
 
 		wp_set_script_translations( 'themeisle-gutenberg-menu-icons', 'textdomain' );
@@ -115,7 +115,7 @@ class GutenbergMenuIcons {
 			'themeisle-gutenberg-menu-icons-frontend',
 			plugin_dir_url( $this->get_dir() ) . $this->slug . '/build/frontend.css',
 			array( 'font-awesome-5' ),
-			$version,
+			$version
 		);
 	}
 
